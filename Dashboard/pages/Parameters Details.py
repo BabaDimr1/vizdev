313da918c0eb0e14e11177cf5bfdaa61d4c45c80
BabaDimr1 <122133760+BabaDimr1@users.noreply.github.com>
2024-05-24 01:44:09 +0200

    Update Parameters Details.py

--- Dashboard/pages/Parameters Details.py
+++ Dashboard/pages/Parameters Details.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Load custom CSS
-with open("styles.css") as f:
+css_file_path = os.path.join(os.path.dirname(__file__), "..", "styles.css")
+with open(css_file_path) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Load the data
