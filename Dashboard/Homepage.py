5fad22b0092f5ae2771424c44a20e18d573597d1
BabaDimr1 <122133760+BabaDimr1@users.noreply.github.com>
2024-05-24 00:57:16 +0200

    Update Homepage.py

--- Dashboard/Homepage.py
+++ Dashboard/Homepage.py
@@ -9,17 +9,19 @@ import os
 # Set page configuration
 st.set_page_config(page_title="Smart Agriculture Dashboard", layout="wide")
 
+
+
+# Load custom CSS
+css_file_path = os.path.join(os.path.dirname(__file__), "styles.css")
+with open(css_file_path) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 # Get the absolute path to the current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Function to construct the file path
 def get_file_path(filename):
     return os.path.join(current_dir, filename)
-
-# Load custom CSS
-css_file_path = os.path.join(os.path.dirname(__file__), "styles.css")
-with open(css_file_path) as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Cache the data loading function
 @st.cache_data
